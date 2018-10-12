73281f3b97e05a1dc73d29a7cfa93785db2ced9b
akforu <34165254+akforu@users.noreply.github.com>
2018-10-13 03:32:31 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,27 +1,146 @@
-package com.example.kalikousik14.test1;
+package com.example.kalikousik14.test2;
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.icu.util.Output;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Node;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-    
+
+    Button btnHit;
+    TextView txtJson;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CardView button = (CardView) findViewById(R.id.button);
-        button.setOnClickListener(
-                new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Intent myIntent = new Intent(getApplicationContext() , Main2Activity.class);
-                startActivity(myIntent);
-            }
-        }
-        );
 
-} }
+        btnHit = (Button) findViewById(R.id.btnHit);
+        txtJson = (TextView) findViewById(R.id.tvJsonItem);
+
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JsonTask().execute("https://api.railwayapi.com/v2/between/source/mdu/dest/dg/date/24-10-2018/apikey/ffctqjbocz/");
+            }
+        });
+
+
+    }
+
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()) {
+                pd.dismiss();
+            }
+            String data="";
+            try {
+                JSONObject jsonRootObject = new JSONObject(result);
+
+                //Get the instance of JSONArray that contains JSONObjects
+                JSONArray jsonArray = jsonRootObject.optJSONArray("trains");
+
+                //Iterate the jsonArray and print the info of JSONObjects
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                     int id = Integer.parseInt(jsonObject.optString("trains_number").toString());
+                     String name = jsonObject.optString("trains_from_station_name").toString();
+                     String salary = jsonObject.optString("trains_classes_name").toString();
+
+                     data += "Node"+i+" : \n id= "+ id +" \n Name= "+ name +" \n Salary= "+ salary +" \n ";
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                DatabaseReference posts = database.getReference();
+                TextView tvJson= (TextView) findViewById(R.id.tvJsonItem);
+                tvJson.setText(data);
+
+            }}
+            catch (JSONException e){e.printStackTrace();}
+        }
+    }
+}
